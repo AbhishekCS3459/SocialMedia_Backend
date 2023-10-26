9ae548f05615b80bf327f4f0dfe9903ccd47a45d
Abhishek Kumar <94506000+AbhishekCS3459@users.noreply.github.com>
2023-10-26 18:48:40 +0530

    Delete dist directory

--- dist/Redis/RedisClient.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ioredis_1 = require("ioredis");
-const RedisClient = new ioredis_1.Redis();
-exports.default = RedisClient;
