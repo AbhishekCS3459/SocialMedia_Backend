fc4378e2e7150bb0afaeaeea3bfc5f4bbeb5676a
Abhishek <abhishekverman3459@gmail.com>
2023-10-30 10:58:09 +0530

    Adding the env redis url

--- dist/Redis/RedisClient.js
+++ dist/Redis/RedisClient.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const ioredis_1 = require("ioredis");
-const RedisClient = new ioredis_1.Redis();
+const redisUrl = process.env.REDIS_URL || "redis://localhost:6379";
+const RedisClient = new ioredis_1.Redis(redisUrl);
 exports.default = RedisClient;
--- src/Redis/RedisClient.ts
+++ src/Redis/RedisClient.ts
@@ -1,5 +1,5 @@
-import {Redis} from 'ioredis'
-
-const RedisClient = new Redis();
+import { Redis } from "ioredis";
+const redisUrl = process.env.REDIS_URL || "redis://localhost:6379";
+const RedisClient = new Redis(redisUrl);
 
 export default RedisClient;
